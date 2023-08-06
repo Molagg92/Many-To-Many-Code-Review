7640ca595bfbb36a96ce9528705bc2005d236f6b
Erik Z <molagg92@gmail.com>
2023-08-05 22:23:17 -0700

    1

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,14 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Factory.Controllers
 {
     public class HomeController : Controller
     {
+           private readonly FactoryContext _db;
+
+      public HomeController(FactoryContext db)
+      {
+        _db = db;
+      }
 
       [HttpGet("/")]
       public ActionResult Index()
-      {
-        return View();
+      { 
+        Engineer[] engs =_db.Engineers.ToArray();
+        Machine[] mechs = _db.Machines.ToArray();
+        Dictionary<string,object[]> model = new Dictionary<string, object[]>();
+        model.Add("engineers", engs);
+        model.Add("machines", mechs);
+        return View(model);
       }
 
     }
---
+++ Factory/Migrations/20230806052036_DTablesFromOtherTables.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Factory.Migrations
+{
+    public partial class DTablesFromOtherTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Machines",
+                keyColumn: "Type",
+                keyValue: null,
+                column: "Type",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "Machines",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Engineers",
+                keyColumn: "Name",
+                keyValue: null,
+                column: "Name",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Engineers",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "Machines",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Engineers",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
